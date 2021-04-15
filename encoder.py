f33160aafba379a4cbfecdddb44a7659a251757b
Baristi000 <ltqtrieu.0204@gmail.com>
2021-04-15 16:26:15 +0700

    change self.data in search to data and add arg:data

--- encoder.py
+++ encoder.py
@@ -2,20 +2,7 @@ import numpy as np
 import requests, faiss
 
 class UniversalEncoder():
-
-    data = [
-    'What color is chameleon?',
-    'When is the festival of colors?',
-    'When is the next music festival?',
-    'How far is the moon?',
-    'How far is the sun?',
-    'What happens when the sun goes down?',
-    'What we do in the shadows?',
-    'What is the meaning of all this?',
-    'What is the meaning of Russel\'s paradox?',
-    'How are you doing?'
-    ]
-
+    
 
     FEATURE_SIZE = 512
     BATCH_SIZE = 32
@@ -54,11 +41,11 @@ class UniversalEncoder():
         faiss.write_index(index,self.storage_dir)
         return index
     
-    def search(self, query, numb_result:int=1):
+    def search(self, data, query, numb_result:int=1):
         index = faiss.read_index(self.storage_dir)
         query_vector = self.encode([query])
         top_k_result = index.search(query_vector, numb_result)
         return [
-            self.data[_id] for _id in top_k_result[1].tolist()[0]
+            data[_id] for _id in top_k_result[1].tolist()[0]
         ]
     
